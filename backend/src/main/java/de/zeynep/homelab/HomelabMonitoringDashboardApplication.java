0cc024f7f099e346f0bb02eda6091981718398fd
Zeynep Ersoezlue <zeynep.ersoezlue@gmail.com>
2026-06-08 18:33:08 +0200

    Spring Boot Backend eingerichtet

---
+++ backend/src/main/java/de/zeynep/homelab/HomelabMonitoringDashboardApplication.java
@@ -0,0 +1,13 @@
+package de.zeynep.homelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelabMonitoringDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelabMonitoringDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/zeynep/homelab/HomelabMonitoringDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.zeynep.homelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelabMonitoringDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
